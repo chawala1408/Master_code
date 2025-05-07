b8d80e46012614ca0c4ad4fa147e146a95fb4134
chawala1408 <69667870+chawala1408@users.noreply.github.com>
2025-05-07 10:04:04 +0700

    Update Master_code-master.ino

--- Master_code-master.ino
+++ Master_code-master.ino
@@ -1,6 +1,5 @@
 #include "MicMMS.h"
 
-#define Version_coding "1.0.1"
 /*------------------- Information Program -------------------*/
 //  MicMMS version 1.0.0  (Version code)
 /*----------------------------------------------------------*/
@@ -8,7 +7,7 @@
 /*------------------- Data list -------------------*/
 // MicMMS aaa(WiFi_name, Password, Mqtt_server, Mqtt_port,/Department/Process/,Machine_number, SlaveID, SerialPort,IP_Address,Gate_way,Subnet_mask,Version_coding);
 /*-------------------------------------------------*/
-MicMMS aaa("MIC_Iot", "Micdev@2024", "192.168.0.128", 1883, "/mic/test/", "a001", 1, Serial1, "192.168.0.100", "192.168.0.1", "255.255.255.0", "1.0.0");
+MicMMS aaa("MIC_Iot", "Micdev@2024", "192.168.0.128", 1883, "/mic/test/", "a001", 1, Serial1, "192.168.0.100", "192.168.0.1", "255.255.255.0", "1.0.1");
 
 void setup() {
   aaa.init();
