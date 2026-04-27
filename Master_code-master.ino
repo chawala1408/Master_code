1a7fcfd168a8a0fcff2aead07ef32968a83c20f9
chawala1408 <69667870+chawala1408@users.noreply.github.com>
2026-04-27 11:38:49 +0700

    Update Master_code-master.ino

--- Master_code-master.ino
+++ Master_code-master.ino
@@ -1,13 +1,14 @@
 #include "MicMMS.h"
-
 /*------------------- Information Program -------------------*/
-//  MicMMS version 1.0.0  (Version code)
+//  MicMMS version 2.0.3  (Version code)
 /*----------------------------------------------------------*/
 
 /*------------------- Data list -------------------*/
-// MicMMS aaa(WiFi_name, Password, Mqtt_server, Mqtt_port,/Department/Process/,Machine_number, SlaveID, SerialPort,IP_Address,Gate_way,Subnet_mask,Version_coding);
+// MicMMS aaa("WiFi_name", "Password", "Mqtt_server", Mqtt_port,"/Department/Process/","Machine_number", 1, Serial1,"IP_Address","Gate_way","Subnet_mask","Version_coding");
 /*-------------------------------------------------*/
-MicMMS aaa("MIC_Iot", "Micdev@2024", "192.168.0.128", 1883, "/mic/test/", "a001", 1, Serial1, "192.168.0.100", "192.168.0.1", "255.255.255.0", "1.0.0");
+
+// MicMMS aaa("MIC-IIoT-DEVICE", "natmms22", "10.128.16.200", 1883,"/nht/gd/","ic45b", 1, Serial1,"10.128.58.155","10.128.58.1","255.255.254.0","2.0.3");
+MicMMS aaa("MIC_Iot", "Micdev@2024", "192.168.0.208", 1883,"/nht/gd/","ic02r", 1, Serial1,"192.168.0.100","192.168.0.1","255.255.255.0","2.0.3"); //MIC are test
 
 void setup() {
   aaa.init();
